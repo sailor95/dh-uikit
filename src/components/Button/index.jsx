ccf8da7a75b724fd2c6d589511b9f1973c0291a0
Dave H <bugman195@gmail.com>
2022-12-10 22:15:43 +0800

    Finalize Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { useColor } from 'hooks/useColor'
+
 const containedStyle = css`
   background: ${props => props.$btnColor};
   color: #fff;
@@ -38,6 +41,20 @@ const variantMap = {
   text: textStyle,
 }
 
+const StyledCircularProgress = styled(CircularProgress)`
+  margin-right: 8px;
+  color: ${props =>
+    props.$variant === 'contained' ? '#FFF' : props.$color} !important;
+`
+
+const StartIcon = styled.span`
+  margin-right: 8px;
+`
+
+const EndIcon = styled.span`
+  margin-left: 8px;
+`
+
 const StyledButton = styled.button`
   border: none;
   outline: none;
@@ -64,14 +81,89 @@ const StyledButton = styled.button`
   }
 `
 
-const Button = ({ children, props }) => (
-  <StyledButton {...props}>
-    <span>{children}</span>
-  </StyledButton>
-)
+/**
+ * `Button` a clickable button, trigger business logic after user click event.
+ */
+const Button = ({
+  className,
+  children,
+  themeColor,
+  variant,
+  isLoading,
+  isDisabled,
+  startIcon,
+  endIcon,
+  onClick,
+  ...props
+}) => {
+  const { makeColor } = useColor()
+  const btnColor = makeColor({ themeColor, isDisabled })
+
+  return (
+    <StyledButton
+      type="button"
+      className={className}
+      $btnColor={btnColor}
+      $variant={variant}
+      disabled={isDisabled}
+      onClick={onClick}
+      {...props}
+    >
+      {isLoading && (
+        <StyledCircularProgress
+          $variant={variant}
+          $color={btnColor}
+          size={16}
+        />
+      )}
+      {startIcon && <StartIcon>{startIcon}</StartIcon>}
+      <span>{children}</span>
+      {endIcon && <EndIcon>{endIcon}</EndIcon>}
+    </StyledButton>
+  )
+}
 
 Button.propTypes = {
+  /**
+   * Button type
+   */
   variant: PropTypes.oneOf(['contained', 'outlined', 'text']),
+  /**
+   * Custom style
+   */
+  className: PropTypes.string,
+  /**
+   * Content
+   */
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.string])
+    .isRequired,
+  /**
+   * Theme color, or custom hex
+   */
+  themeColor: PropTypes.oneOfType([
+    PropTypes.oneOf(['primary', 'secondary']),
+    PropTypes.string,
+  ]),
+  /**
+   * Loading state
+   */
+  isLoading: PropTypes.bool,
+  /**
+   * Disable state
+   */
+  isDisabled: PropTypes.bool,
+  /**
+   * Icon on the left
+   */
+  startIcon: PropTypes.element,
+  /**
+   * Icon on the right
+   */
+  endIcon: PropTypes.element,
+  /**
+   * Click event
+   */
+  onClick: PropTypes.func,
 }
 
 export default Button
