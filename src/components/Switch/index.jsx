b10888c6239b5733a61c4ff451cba353b3e1fb0d
Dave H <bugman195@gmail.com>
2023-03-02 16:53:29 +0800

    Init Switch

---
+++ src/components/Switch/index.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const SwitchButton = styled.div``
+
+const Switch = ({
+  isChecked,
+  isDisabled,
+  themeColor,
+  onChange,
+  size,
+  checkedChildren,
+  unCheckedChildren,
+  ...props
+}) => {
+  return <SwitchButton></SwitchButton>
+}
+
+Switch.propTypes = {
+  /**
+   * Check state
+   */
+  isChecked: PropTypes.bool,
+  /**
+   * Disable state
+   */
+  isDisabled: PropTypes.bool,
+  /**
+   * Theme color, or custom hex
+   */
+  themeColor: PropTypes.oneOfType([
+    PropTypes.oneOf(['primary', 'secondary']),
+    PropTypes.string,
+  ]),
+  /**
+   * State change callback
+   */
+  onChange: PropTypes.func,
+  /**
+   * Size of the Switch
+   */
+  size: PropTypes.string,
+  /**
+   * Children for checked
+   */
+  checkedChildren: PropTypes.string,
+  /**
+   * Children for unchecked
+   */
+  unCheckedChildren: PropTypes.string,
+}
+
+Switch.defaultProps = {
+  isChecked: null,
+  isDisabled: null,
+  themeColor: 'primary',
+  size: 'default',
+  checkedChildren: '',
+  unCheckedChildren: '',
+  onChange: () => {},
+}
+
+export default Switch
