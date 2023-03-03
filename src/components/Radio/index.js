6fc13d263ea9b6a979613870c10715c07c76569a
Dave H <bugman195@gmail.com>
2023-03-03 19:34:02 +0800

    Finalize Radio

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -1,7 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-const Radio = props => <div>Radio</div>
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked'
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked'
+
+import { useColor } from 'hooks/useColor'
+
+const DISABLED_COLOR = '#dadada'
+
+const StyledRadio = styled.div`
+  display: inline-flex;
+  align-items: center;
+  cursor: ${props => (props.$isDisabled ? 'not-allowed' : 'pointer')};
+  color: ${props => (props.$isDisabled ? DISABLED_COLOR : '#222222')};
+  & > *:not(:first-child) {
+    margin-left: 8px;
+  }
+  .radio__checked-icon {
+    color: ${props => props.$btnColor};
+  }
+  .radio__unchecked-icon {
+    color: ${props => (props.$isDisabled ? DISABLED_COLOR : '#808080')};
+  }
+  &:hover {
+    .radio__unchecked-icon {
+      color: ${props => (props.$isDisabled ? DISABLED_COLOR : props.$btnColor)};
+    }
+  }
+`
+
+const Radio = ({
+  isChecked,
+  isDisabled,
+  themeColor,
+  onClick,
+  value,
+  children,
+  ...props
+}) => {
+  const { makeColor } = useColor()
+  const btnColor = makeColor({ themeColor, isDisabled })
+
+  return (
+    <StyledRadio
+      onClick={isDisabled ? null : onClick}
+      $isDisabled={isDisabled}
+      $btnColor={btnColor}
+      {...props}
+    >
+      {isChecked ? (
+        <RadioButtonCheckedIcon className="radio__checked-icon" />
+      ) : (
+        <RadioButtonUncheckedIcon className="radio__unchecked-icon" />
+      )}
+      <span>{children}</span>
+    </StyledRadio>
+  )
+}
 
 Radio.propTypes = {
   /**
