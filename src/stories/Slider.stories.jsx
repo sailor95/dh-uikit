0106e0d84fcd20a6a2f18f2f2cc8583144aa2b47
Dave H <bugman195@gmail.com>
2023-03-14 12:54:42 +0800

    Add Slider stories

---
+++ src/stories/Slider.stories.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import Slider from '../components/Slider'
+
+export default {
+  title: 'Data Inputs/Slider',
+  component: Slider,
+}
+
+const SliderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  & > *:not(:first-child) {
+    margin-left: 20px;
+  }
+`
+
+const Template = args => {
+  const { defaultValue } = args
+  const [value, setValue] = useState(defaultValue || 0)
+
+  return (
+    <SliderWrapper>
+      <Slider
+        {...args}
+        onChange={event => {
+          setValue(event.target.value)
+        }}
+      />
+      <span>{value}</span>
+    </SliderWrapper>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const CustomColor = Template.bind({})
+CustomColor.args = {
+  defaultValue: 50,
+  themeColor: '#42f5c5',
+}
+
+export const WithStep = Template.bind({})
+WithStep.args = {
+  min: 0,
+  max: 8,
+  step: 2,
+}
+
+export const WithDefaultValue = Template.bind({})
+WithDefaultValue.args = {
+  defaultValue: 50,
+}
