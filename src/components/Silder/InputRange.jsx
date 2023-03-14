53711cb2bf500d6f83512f8a417e3c23a422b684
Dave H <bugman195@gmail.com>
2023-03-14 12:45:37 +0800

    Finalize CSS

--- src/components/Silder/InputRange.jsx
+++ src/components/Silder/InputRange.jsx
@@ -1,8 +1,65 @@
 import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const StyledSlider = styled.input``
+import { useColor } from 'hooks/useColor'
+
+const THUMB_SIZE = 20
+
+const railStyle = css`
+  background: #ddd;
+  width: 320px;
+  height: 6px;
+  border-radius: 5px;
+`
+
+const trackStyle = css`
+  background: ${props => props.$color};
+  border-radius: 5px;
+  height: 6px;
+`
+
+const StyledSlider = styled.input`
+  &[type='range'] {
+    -webkit-appearance: none;
+    outline: none;
+    position: relative;
+    z-index: 0;
+    ${railStyle}
+
+    &:before {
+      content: '';
+      position: absolute;
+      z-index: -1;
+      width: ${props => props.$widthRatio}%;
+      left: 0px;
+      ${trackStyle}
+    }
+  }
+
+  // thumb style
+  &[type='range']::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: ${THUMB_SIZE}px;
+    height: ${THUMB_SIZE}px;
+    border-radius: 50%;
+    border: 2px solid white;
+    background: white;
+    border: 0.4em solid ${props => props.$color};
+    cursor: pointer;
+    transition: box-shadow 0.2s ease-in-out, transform 0.1s ease-in-out;
+    &:hover {
+      transform: scale(1.1);
+      box-shadow: 0 0.4em 1em rgba(0, 0, 0, 0.15);
+    }
+    &:active {
+      cursor: grabbing;
+      transform: scale(0.975);
+      box-shadow: 0 0 1px rgba(0, 0, 0, 0.1);
+      background: ${props => props.$color};
+    }
+  }
+`
 
 const Slider = ({
   defaultValue,
@@ -15,6 +72,8 @@ const Slider = ({
 }) => {
   const [currentValue, setCurrentValue] = useState(defaultValue)
   const sliderRef = useRef()
+  const { makeColor } = useColor()
+  const color = makeColor({ themeColor })
 
   const handleOnChange = event => {
     setCurrentValue(sliderRef.current.value)
@@ -30,6 +89,8 @@ const Slider = ({
       step={step}
       defaultValue={defaultValue}
       onChange={handleOnChange}
+      $widthRatio={(currentValue / max) * 100}
+      $color={color}
       {...props}
     />
   )
