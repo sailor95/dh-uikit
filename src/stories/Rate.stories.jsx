0efcc672495ee392c538fc92729500c7a85d1505
Dave H <bugman195@gmail.com>
2023-03-17 16:48:38 +0800

    Add Rate stories

---
+++ src/stories/Rate.stories.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import SentimentVerySatisfiedOutlinedIcon from '@material-ui/icons/SentimentVerySatisfiedOutlined'
+
+import Rate from '../components/Rate'
+
+export default {
+  title: 'Data Inputs/Rate',
+  component: Rate,
+}
+
+const Template = args => <Rate {...args} />
+
+const Row = styled.div`
+  display: flex;
+  align-items: center;
+  & > *:not(:first-child) {
+    margin-left: 12px;
+  }
+`
+
+const TemplateWithState = args => {
+  const [value, setValue] = useState(3.5)
+  return (
+    <Row>
+      <Rate {...args} onChange={setValue} />
+      <div>{value}</div>
+    </Row>
+  )
+}
+
+const CharacterGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > *:not(:first-child) {
+    margin-top: 20px;
+  }
+`
+
+const TemplateWithDiffCharacter = args => (
+  <CharacterGroup>
+    <Rate {...args} character={<FavoriteBorderIcon />} />
+    <Rate {...args} character={<SentimentVerySatisfiedOutlinedIcon />} />
+    <Rate {...args} character="Y" />
+  </CharacterGroup>
+)
+
+const TemplateWithCount = args => (
+  <CharacterGroup>
+    <Rate {...args} count={1} />
+    <Rate {...args} count={3} />
+    <Rate {...args} count={5} />
+    <Rate {...args} count={7} />
+  </CharacterGroup>
+)
+
+export const Default = Template.bind({})
+Default.args = {}
+
+export const WithDefaultValue = Template.bind({})
+WithDefaultValue.args = {
+  defaultValue: 2.5,
+}
+
+export const WithCount = TemplateWithCount.bind({})
+WithCount.args = {}
+
+export const DisableInteraction = Template.bind({})
+DisableInteraction.args = {
+  defaultValue: 4,
+  isDisabled: true,
+}
+
+export const WithOnChange = TemplateWithState.bind({})
+WithOnChange.args = {
+  defaultValue: 3,
+}
+
+export const AllowHalf = TemplateWithState.bind({})
+AllowHalf.args = {
+  defaultValue: 3.5,
+  allowHalf: true,
+}
+
+export const WithSize = Template.bind({})
+WithSize.args = {
+  defaultValue: 3,
+  size: 48,
+}
+
+export const WithColor = Template.bind({})
+WithColor.args = {
+  defaultValue: 3,
+  themeColor: '#ff389c',
+}
+
+export const CustomCharacter = TemplateWithDiffCharacter.bind({})
+CustomCharacter.args = {
+  allowHalf: true,
+  defaultValue: 2.5,
+}
