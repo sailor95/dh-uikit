804802c1cfc16afe73c65cdd51505cd357c1f3ed
Dave H <bugman195@gmail.com>
2023-03-17 16:36:23 +0800

    Finalize Rate logic

--- src/components/Rate/index.js
+++ src/components/Rate/index.js
@@ -1,9 +1,41 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react'
+import styled, { css } from 'styled-components'
 import PropTypes from 'prop-types'
 import StarIcon from '@material-ui/icons/Star'
 
-const RateContainer = styled.div``
+import { useColor } from 'hooks/useColor'
+
+const RateContainer = styled.div`
+  display: inline-flex;
+  ${props => (props.$isString ? `font-size: ${props.$size}px` : null)}
+`
+
+const CharacterContainer = styled.div`
+  position: relative;
+`
+
+const characterCommonStyle = css`
+  ${props => (props.$isString ? null : `height: ${props.$size}px;`)}
+  & > * {
+    width: ${props => props.$size}px !important;
+    height: ${props => props.$size}px !important;
+  }
+  cursor: pointer;
+  color: ${props => (props.$isActive ? props.$starColor : '#F0F0F0')};
+`
+
+const LeftCharacter = styled.div`
+  ${characterCommonStyle}
+  ${props => (props.$allowHalf ? null : 'display: none;')}
+
+  position: absolute;
+  width: 50%;
+  overflow: hidden;
+`
+
+const RightCharacter = styled.div`
+  ${characterCommonStyle}
+`
 
 const Rate = ({
   count,
@@ -15,7 +47,68 @@ const Rate = ({
   allowHalf,
   onChange,
 }) => {
-  return <RateContainer>Rate</RateContainer>
+  const isString = typeof character === 'string'
+
+  const [innerValue, setInnerValue] = useState(defaultValue)
+  const [previewValue, setPreviewValue] = useState(innerValue)
+
+  const { makeColor } = useColor()
+  const starColor = makeColor({ themeColor })
+
+  const handleOnClick = value => {
+    if (isDisabled) return
+    setInnerValue(prev => (prev === value ? 0 : value))
+  }
+
+  const handleChangePreviewValue = value => {
+    if (!isDisabled) {
+      setPreviewValue(value)
+    }
+  }
+
+  useEffect(() => {
+    if (typeof onChange === 'function') {
+      onChange(innerValue)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [innerValue])
+
+  const renderRates = () =>
+    [...Array(count).keys()].map(key => (
+      <CharacterContainer key={key}>
+        <LeftCharacter
+          className="rate_character-left"
+          $size={size}
+          $isString={isString}
+          $starColor={starColor}
+          $isActive={key + 0.5 <= previewValue}
+          $allowHalf={allowHalf}
+          onClick={() => handleOnClick(key + 0.5)}
+          onMouseOver={() => handleChangePreviewValue(key + 0.5)}
+          onMouseLeave={() => handleChangePreviewValue(innerValue)}
+        >
+          {character}
+        </LeftCharacter>
+        <RightCharacter
+          className="rate_character-right"
+          $size={size}
+          $isString={isString}
+          $starColor={starColor}
+          $isActive={key + 1 <= previewValue}
+          onClick={() => handleOnClick(key + 1)}
+          onMouseOver={() => handleChangePreviewValue(key + 1)}
+          onMouseLeave={() => handleChangePreviewValue(innerValue)}
+        >
+          {character}
+        </RightCharacter>
+      </CharacterContainer>
+    ))
+
+  return (
+    <RateContainer $size={size} $isString={isString}>
+      {renderRates()}
+    </RateContainer>
+  )
 }
 
 Rate.propTypes = {
